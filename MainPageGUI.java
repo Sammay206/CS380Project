a1f09b011e85035dfae03dd505aee449e29296b2
JalixaC <146808895+JalixaC@users.noreply.github.com>
2023-11-19 23:35:31 -0800

    Updated MainPageGUI.java with square implementation
    added squareGUI class to MainPageGUI to create squares with the switch statement

--- MainPageGUI.java
+++ MainPageGUI.java
@@ -1,5 +1,6 @@
 package project380;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -11,6 +12,12 @@ public class MainPageGUI {
 
 	private User currentUser;
 	private JFrame frame;
+	private JLabel Expense1;
+	private JLabel Expense2; 
+	private JLabel Expense3; 
+	private JLabel Expense4;
+	private JLabel Expense5; 
+	private JLabel Expense6; 
 
 	/**
 	 * Create the application.
@@ -49,6 +56,64 @@ public class MainPageGUI {
 		lblHello.setBounds(10, 10, 340, 35);
 		lblHello.setText("Hello, " + currentUser.getUsername());
 		frame.getContentPane().add(lblHello);
+
+		//Adding squares (max 6) with their labels according to how many nodes are in array
+		ExpenseType[] temp = new ExpenseType[currentUser.getNodes().length];
+		for(int i=0; i < temp.length; i++) {
+			temp[i].toString();
+			switch(i) {
+				case 0:
+					//square 1
+					squareGUI s1 = new squareGUI(temp[i].evaluateGoal(), 100, 100);
+					frame.getContentPane().add(s1);
+					Expense1 = new JLabel("Node 1");
+					Expense1.setBounds(75, 95, 100, 16);
+					Expense1.setText(currentUser.getNodes()[i].toString());
+					frame.getContentPane().add(Expense1);
+					
+					break;
+				case 1:
+					//square 2 
+					frame.getContentPane().add(new squareGUI(temp[i].evaluateGoal(), 310, 100));
+					Expense2 = new JLabel("Node 2");
+					Expense2.setBounds(75, 95, 100, 16);
+					Expense2.setText(currentUser.getNodes()[i].toString());
+					break;
+				case 2:
+					//square 3
+					frame.getContentPane().add(new squareGUI(temp[i].evaluateGoal(), 510, 100));
+					Expense3 = new JLabel("Node 3");
+					Expense3.setBounds(75, 95, 100, 16);
+					Expense3.setText(currentUser.getNodes()[i].toString());
+					break;
+				case 3:
+					//square 4
+					frame.getContentPane().add(new squareGUI(temp[i].evaluateGoal(), 100, 310));
+					Expense4 = new JLabel("Node 4");
+					Expense4.setBounds(75, 95, 100, 16);
+					Expense4.setText(currentUser.getNodes()[i].toString());
+					break;
+				case 4:
+					//square 5
+					frame.getContentPane().add(new squareGUI(temp[i].evaluateGoal(), 310, 310));
+					Expense5 = new JLabel("Node 5");
+					Expense5.setBounds(75, 95, 100, 16);
+					Expense5.setText(currentUser.getNodes()[i].toString());
+					
+					break;
+				case 5:
+					//square 6
+					frame.getContentPane().add(new squareGUI(temp[i].evaluateGoal(), 510, 310));
+					Expense6 = new JLabel("Node 6");
+					Expense6.setBounds(75, 95, 100, 16);
+					Expense6.setText(currentUser.getNodes()[i].toString());
+					break;
+			}
+		}
+
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		
 	}
 }
