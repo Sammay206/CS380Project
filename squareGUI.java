e0074ebdffe44ae9e5cdbdc3729c2e56960c3428
JalixaC <146808895+JalixaC@users.noreply.github.com>
2023-11-26 13:50:30 -0800

    Updated squareGUI.java with better look

--- squareGUI.java
+++ squareGUI.java
@@ -11,72 +11,124 @@ public class squareGUI extends JComponent{
 	String color; 
 	String [] colors = new String[6];
 	ExpenseType[] expenseString; 
+	User currentU;
+	Graphics2D g2;
 	
 	
-	squareGUI(String[] arrColor, ExpenseType[] temp){
+	squareGUI( String[] arrColor, ExpenseType[] temp, User currU){
 		colors = arrColor;
 		expenseString = temp;
+		this.currentU = currU;
 	}
-	
+	/**
+	 * colorSwitch() is the method that chooses the color for the square
+	 * according to the color in the colors array
+	 */
 	public void colorSwitch(Graphics g, String newColor) {
 		switch(newColor) {
-		case "red":
-			g.setColor(Color.red);
+		case "Red":
+			g.setColor(new Color(240,72,50));
 			break; 
-		case "green":
-			g.setColor(Color.green);
+		case "Green":
+			g.setColor(new Color(158, 235, 63));
 			break; 
-		case "yellow":
-			g.setColor(Color.yellow);
+		case "Yellow":
+			g.setColor(new Color(247, 211, 2));
 			break;
 	}
 	}
 	
-	public void paint(Graphics g) {
+	protected void paintComponent(Graphics g) {
 		
 	Graphics2D g2 = (Graphics2D) g;	
 	
+	g2.setFont(new Font("Tahoma", Font.BOLD| Font.ITALIC, 25));
+	g2.setColor(Color.black);
+	g2.drawString("Hello, " + currentU.getUsername(), 60,60);
+	g2.setFont(new Font("Helvetica", Font.PLAIN, 18));
+	g2.drawString("let's see if you maintained your goals", 60,85);
+	
 	for(int i =0; i < colors.length; i++) {
-		//should draw the square with hard coded positions with labels of ExpenseType 
+		//should draw the square with hard coded positions with labels of ExpesneType 
+		
+		if(colors[i] == null) {
+			break;
+		}
+		
 		switch(i) {
 		case 0: 
 			//Square 1 
-			g2.drawString(expenseString[i].toString(), 115, 150);
 			colorSwitch(g2,colors[i]);
 			g2.fillRect(100, 100, 200, 200);
+			g2.setColor(Color.black);
+			g2.setFont(new Font("Tahoma", Font.PLAIN, 35));
+			g2.drawString(expenseString[i].getName(), 115, 150);
+			
+			g2.setFont(new Font("Tahoma", Font.ITALIC, 20));
+			g2.drawString("Goal: " +expenseString[i].getGoal(), 115, 230);
+			g2.drawString("Expense: "+ expenseString[i].getExpenses(),115, 250);
 			break; 
 		case 1:
 			//Square 2
-			g2.drawString(expenseString[i].toString(), 325, 150);
 			colorSwitch(g2, colors[i]);
 			g2.fillRect(310, 100, 200, 200);
+			g2.setColor(Color.black);
+			g2.setFont(new Font("Tahoma", Font.PLAIN, 35));
+			g2.drawString(expenseString[i].getName(), 325, 150);
+			
+			g2.setFont(new Font("Tahoma", Font.ITALIC, 20));
+			g2.drawString("Goal: " +expenseString[i].getGoal(), 325, 230);
+			g2.drawString("Expense: "+ expenseString[i].getExpenses(),325, 250);
 			break;
 		case 2:
 			//Square 3
-			g2.drawString(expenseString[i].toString(), 525, 150);
 			colorSwitch(g2, colors[i]);
-			g2.fillRect(510, 100, 200, 200);
+			g2.fillRect(520, 100, 200, 200);
+			g2.setColor(Color.black);
+			g2.setFont(new Font("Tahoma", Font.PLAIN, 35));
+			g2.drawString(expenseString[i].getName(), 535, 150);
+			
+			g2.setFont(new Font("Tahoma", Font.ITALIC, 20));
+			g2.drawString("Goal: " +expenseString[i].getGoal(), 535, 230);
+			g2.drawString("Expense: "+ expenseString[i].getExpenses(),535, 250);
 			break;
 		case 3:
 			//Square 4
-			g2.drawString(expenseString[i].toString(), 115, 410);
 			colorSwitch(g2, colors[i]);
 			g2.fillRect(100, 310, 200, 200);
+			g2.setColor(Color.black);
+			g2.setFont(new Font("Tahoma", Font.PLAIN, 35));
+			g2.drawString(expenseString[i].getName(), 115, 360);
+			
+			g2.setFont(new Font("Tahoma", Font.ITALIC, 20));
+			g2.drawString("Goal: " +expenseString[i].getGoal(), 115, 440);
+			g2.drawString("Expense: "+ expenseString[i].getExpenses(),115, 460);
 			break;
 		case 4:
 			//Square 5
-			g2.drawString(expenseString[i].toString(), 325, 410);
 			colorSwitch(g2, colors[i]);
 			g2.fillRect(310, 310, 200, 200);
+			g2.setColor(Color.black);
+			g2.setFont(new Font("Tahoma", Font.PLAIN, 35));
+			g2.drawString(expenseString[i].getName(), 325, 360);
+			
+			g2.setFont(new Font("Tahoma", Font.ITALIC, 20));
+			g2.drawString("Goal: " +expenseString[i].getGoal(), 325, 440);
+			g2.drawString("Expense: "+ expenseString[i].getExpenses(),325, 460);
 			break;
 		case 5:
 			//Square 6
-			g2.drawString(expenseString[i].toString(), 525, 410);
 			colorSwitch(g2, colors[i]);
-			g2.fillRect(510, 310, 200, 200);
+			g2.fillRect(520, 310, 200, 200);
+			g2.setColor(Color.black);
+			g2.setFont(new Font("Tahoma", Font.PLAIN, 35));
+			g2.drawString(expenseString[i].getName(), 535, 360);
+			
+			g2.setFont(new Font("Tahoma", Font.ITALIC, 20));
+			g2.drawString("Goal: " +expenseString[i].getGoal(), 535, 440);
+			g2.drawString("Expense: "+ expenseString[i].getExpenses(),535, 460);
 			break;
 		}
-	}
-	} 
+	}} 
 
 }
